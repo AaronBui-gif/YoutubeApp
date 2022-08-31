60242a75cb1fd1d1bc375b2861cbcd5ebb15d3a9
AaronBui-gif <s3740934@rmit.edu.vn>
2022-09-01 00:10:20 +0700

    Initial Commit

---
+++ YoutubeApp/YoutubeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeAppApp.swift
+//  YoutubeApp
+//
+//  Created by Huy Bui Thanh on 01/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
