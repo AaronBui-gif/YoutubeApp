b77ac392513d3a0b5ead6b543b06a5958201a0bd
AaronBui-gif <s3740934@rmit.edu.vn>
2022-09-07 01:05:07 +0700

    update

--- YoutubeApp/ContentView.swift
+++ YoutubeApp/ContentView.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showYoutube = false
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VideoView(videoID: "Go8nTmfrQd8")
     }
 }
 
---
+++ YoutubeApp/VideoView.swift
@@ -0,0 +1,24 @@
+//
+//  VideoView.swift
+//  YoutubeApp
+//
+//  Created by Huy Bui Thanh on 01/09/2022.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+struct VideoView: UIViewRepresentable {
+    let videoID: String
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        guard let youtubeURL = URL(string: "https://www.youtube.com/embed/\(videoID)") else {return }
+        uiView.scrollView.isScrollEnabled = false
+        uiView.load(URLRequest(url: youtubeURL))
+    }
+}
